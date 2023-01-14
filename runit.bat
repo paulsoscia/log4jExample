25d6d4a2862b4e1de3065f53e833e2f2e63d8b81
paul soscia <paulsoscia@gmail.com>
2023-01-14 17:48:50 -0500

    added LOCAL to try to preserve ClassPath and Path
    added LOCAL to try to preserve ClassPath and Path

--- runit.bat
+++ runit.bat
@@ -1,15 +1,31 @@
 cls 
 
+echo "global classpath"
 echo %classpath%
+echo ""
+echo "global path"
 echo %path%
 
-set CLASSPATH=%CLASSPATH%;"\\PENNRIVER001\Users\Tmp;.;C:\Users\PaulS\JavaCode\log4jExample\*;C:\Users\PaulS\JavaCode\log4jExample\;C:\Users\PaulS\JavaCode\log4jExample;C:\Users\PaulS\JavaCode\log4jExample\log4j.properties;"
+SETLOCAL
 
-set PATH=\\PENNRIVER001\Users\Tmp;%PATH%;C:\Users\PaulS\JavaCode\log4jExample
+  set CLASSPATH=%CLASSPATH%;"\\PENNRIVER001\Users\Tmp;.;C:\Users\PaulS\JavaCode\log4jExample\*;C:\Users\PaulS\JavaCode\log4jExample\;C:\Users\PaulS\JavaCode\log4jExample;C:\Users\PaulS\JavaCode\log4jExample\log4j.properties;"
 
-"C:\Program Files\Java\jdk-19\bin\javac" log4jExample.java
+  set PATH=\\PENNRIVER001\Users\Tmp;%PATH%;C:\Users\PaulS\JavaCode\log4jExample
 
+  "C:\Program Files\Java\jdk-19\bin\javac" log4jExample.java
 
-java -Dlog4j2.debug=true -Dlog4j.configurationFile=file:C:\Users\PaulS\JavaCode\log4jExample\log4j.properties log4jExample
+  java -Dlog4j2.debug=true -Dlog4j.configurationFile=file:C:\Users\PaulS\JavaCode\log4jExample\log4j.properties log4jExample
+
+  echo "local classpath"
+  echo %classpath%
+  echo ""
+  echo "local path"
+  echo %path%
+
+ENDLOCAL
+
+echo "global classpath"
 echo %classpath%
+echo ""
+echo "global path"
 echo %path%
